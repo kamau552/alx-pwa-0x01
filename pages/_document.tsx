46d7547c647fbf35c279ad98f03826fa5b652954
Njeri Kamau <faithkamau730@gmail.com>
2025-07-06 18:51:28 +0300

    Create a Web App Manifest

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,12 +2,16 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
-      <body className="antialiased">
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
+<Html lang="en">
+  <Head>
+    <link rel="manifest" href="/manifest.json" />
+    <link rel="apple-touch-icon" href="/icons/apple-icon-152x152.png" />
+    <meta name="theme-color" content="#FFFFFF" />
+  </Head>
+  <body className="antialiased">
+    <Main />
+    <NextScript />
+  </body>
+</Html>
   );
 }
