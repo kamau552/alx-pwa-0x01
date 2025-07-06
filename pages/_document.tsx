da870584bb4144cb4a0f9b0c860908f0926fe8cc
Njeri Kamau <faithkamau730@gmail.com>
2025-07-06 19:28:22 +0300

    Link the Manifest in _document.js

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,16 +2,17 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-<Html lang="en">
-  <Head>
-    <link rel="manifest" href="/manifest.json" />
-    <link rel="apple-touch-icon" href="/icons/apple-icon-152x152.png" />
-    <meta name="theme-color" content="#FFFFFF" />
-  </Head>
-  <body className="antialiased">
-    <Main />
-    <NextScript />
-  </body>
-</Html>
+    <Html lang="en">
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#0070f3" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      </Head>
+      <body className="antialiased">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
   );
 }
+
